29ae7026a8e30fa079f90baa45c5d979128c3f38
Billy Zimmerman <billyzman87@gmail.com>
2023-02-24 12:17:44 -0600

    added special attack function with a computed property that only allows for a special attack if the turn is divisible by 3

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,9 +7,9 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
-
   computed: {
     monsterBarStyles() {
       return { width: this.monsterHealth + "%" };
@@ -17,17 +17,28 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
-      this.monsterHealth = this.monsterHealth - attackValue;
+      this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
-      this.playerHealth = this.playerHealth - attackValue;
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
+
 app.mount("#game");
