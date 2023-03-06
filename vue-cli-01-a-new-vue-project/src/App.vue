eeaba46028854c7989bd64547b31a7170818ea39
Billy Zimmerman <billyzman87@gmail.com>
2023-03-06 14:44:09 -0600

    added 'app.vue' file to start to import components to

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "kelsey",
+          name: "kelsey zimmerman",
+          phone: "123-432-9768",
+          email: "kelsey@aol.com",
+        },
+        {
+          id: "luis",
+          name: "Luis Torres",
+          phone: "393-382-9768",
+          email: "luis@aol.com",
+        },
+      ],
+    };
+  },
+};
+</script>
