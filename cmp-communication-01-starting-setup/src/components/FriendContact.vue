8dbb9db9e847f6302308961b243fea4bd1907939
Billy Zimmerman <billyzman87@gmail.com>
2023-03-08 14:44:34 -0600

    added props to FriendContact component and making progress on making this starter app more dynamic

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,72 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Best Friend )" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "kelsey",
+        name: "Kelsey Zimmerman",
+        phone: "012 3456 7890",
+        email: "kelsey@localhost.com",
+      },
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
+      }
+    },
+  },
+};
+</script>
