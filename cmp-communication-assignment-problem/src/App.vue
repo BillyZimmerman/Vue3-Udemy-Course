2bfce63bb0e8bdb179113a91023604df5edc858e
Billy Zimmerman <billyzman87@gmail.com>
2023-03-09 15:49:58 -0600

    pushing components challenge

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <div class="parent">
+      <div class="child">
+        <active-user :username="user.name" :userAge="user.age"></active-user>
+      </div>
+
+      <div class="child">
+        <user-data @set-data="setUserData"></user-data>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Billy Zimmerman",
+        age: 35,
+      },
+    };
+  },
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name: name,
+        age: +age,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.parent {
+  border: 1px solid black;
+  margin: 1rem;
+  padding: 2rem 2rem;
+  text-align: center;
+}
+.child {
+  display: inline-block;
+  border: 1px solid rgb(10, 24, 49);
+  padding: 1rem 1rem;
+  vertical-align: middle;
+  width: 10rem;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <h1>Active User</h1> -->
+  <div>
+    <h2>{{ username }}</h2>
+    <h3>{{ userAge }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userAge: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
